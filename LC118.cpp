ce6c420ae0524ff73b71a9be17dd08716b8fbbac
asmodaks30 <apurva.modak.spam@gmail.com>
2025-06-01 14:53:26 -0400

    LC118

---
+++ LC118.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> results;
+        for (int i=1; i<=numRows; i++) {
+            results.push_back(vector<int>(i, 1));
+            for (int j = 1; j <= i-2; j++) {
+                results[i-1][j] = results[i-2][j-1] + results[i-2][j];
+            }
+        }
+        return results;
+    }
+};
